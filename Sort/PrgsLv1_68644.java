e7ac69b82ddf5612560932517b64b85d44337c82
goldanghenry <wshdhkd@naver.com>
2025-01-28 21:01:02 +0900

    PrgsLv1_68644

---
+++ Sort/PrgsLv1_68644.java
@@ -0,0 +1,19 @@
+package Sort;
+import java.util.HashSet;
+/*
+    programmers 두 개 뽑아서 더하기
+    https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java
+
+ */
+public class PrgsLv1_68644 {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length -1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
