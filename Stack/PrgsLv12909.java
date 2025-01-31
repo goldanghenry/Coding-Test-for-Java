46f7e5df82f7a4fc746ff4b0646ea3d26dfec709
우성현 <wshdhkd@naver.com>
2025-01-31 15:42:43 +0900

    PrgsLv2_12909

---
+++ Stack/PrgsLv12909.java
@@ -0,0 +1,28 @@
+package Stack;
+
+import java.util.ArrayDeque;
+
+/*
+ * programmers Lv2 올바른 괄호
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
+ */
+
+class PrgsLv12909 {
+    public static boolean solution(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        
+        char[] arr = s.toCharArray();
+        for( char c : arr) {
+            if ( c == '(') {
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
