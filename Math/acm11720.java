fd5018bdfb4231ac6798218b1dbc961e275bfa9f
우성현 <wshdhkd@naver.com>
2025-02-05 17:56:30 +0900

    acm11720

---
+++ Math/acm11720.java
@@ -0,0 +1,23 @@
+package Math;
+
+/*
+ * 숫자의 합 bronze 4
+ * https://www.acmicpc.net/problem/11720
+ */
+
+import java.io.*;
+
+public class acm11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        String numbers = br.readLine();
+
+        int sum = 0;
+        for (int i = 0; i < N; i++ ) {
+            sum += numbers.charAt(i) -'0';
+        }
+        System.out.println(sum);
+    }
+}
