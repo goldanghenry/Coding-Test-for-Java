992b028fe29d97ec18c11f65fb57375880991bf7
goldanghenry <wshdhkd@naver.com>
2025-02-06 21:23:21 +0900

    graph search

---
+++ Basic/BFSExample.java
@@ -0,0 +1,42 @@
+package Basic;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFSExample {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while( !queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int next : graph.get(node)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        graph = new ArrayList<>();
+        visited = new boolean[N+1];
+
+        for (int i = 0; i < N+1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        graph.get(0).add(0);
+    }
+
+
+}
---
+++ Basic/DFSExample.java
@@ -0,0 +1,39 @@
+package Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DFSExample {
+    public static List<List<Integer>> graph;
+    public static boolean[] visited;
+
+    public static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node+" ");
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        graph = new ArrayList<>();
+        visited = new boolean[N+1];
+
+        for (int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(2).add(1);
+        graph.get(2).add(4);
+        graph.get(3).add(1);
+        graph.get(4).add(2);
+
+        dfs(1);
+
+    }
+}
