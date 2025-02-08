b551c9f1c6bcccfa4748e31c05872fd36a491ee1
goldanghenry <wshdhkd@naver.com>
2025-02-08 17:15:26 +0900

    Boj_S1_2178

---
+++ Graph/Boj_S1_2178.java
@@ -0,0 +1,71 @@
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+    Boj Silver 1, 미로 탐색
+    https://www.acmicpc.net/problem/2178
+    최단 경로 탐색이므로 BFS를 통해 거리를 기록하며 탐색을 진행
+ */
+
+public class Boj_S1_2178 {
+    static int N,M;
+    static int[][] maze;   // 맵이므로 ArrayList 보다는 matrix로
+    static boolean[][] visited;
+    static int[][] dist;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        maze = new int[N][M];           // 맵
+        visited = new boolean[N][M];    // 방문 체크
+        dist = new int[N][M];
+
+        // 맵 입력받기
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = input.charAt(j) -'0';
+            }
+        }
+
+        // BFS 탐색 시작
+        System.out.println(bfs(0,0));
+    }
+    static int bfs(int x, int y) {
+        // 큐 선언
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x,y});
+        visited[x][y] = true;
+        dist[x][y] = 1;
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                // 맵을 벗어나지 않았고, 이동가능하고(1), 방문하지 않았다면(false) 방문
+                if ( nx >= 0 && ny >= 0 && nx < N && ny < M &&
+                        maze[nx][ny] == 1 && !visited[nx][ny] ) {
+                    queue.add(new int[]{nx,ny});
+                    visited[nx][ny] = true;
+                    dist[nx][ny] = dist[x][y]+1;
+                }
+            }
+        }
+        return dist[N-1][M-1];
+    }
+}
---
+++ Graph/Boj_S2_1260.java
@@ -0,0 +1,84 @@
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+    Boj Silver 2, DFS와 BFS
+    https://www.acmicpc.net/problem/1260
+ */
+
+public class Boj_S2_1260 {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int node) {
+        visited[node] = true;
+        sb.append(node).append(" ");
+
+        for( int next : graph.get(node)) {
+            if(!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    static void bfs(int node) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(node);
+        visited[node] = true;
+        sb.append(node).append(" ");
+
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for(int next : graph.get(current)) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                    sb.append(next).append(" ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        visited = new boolean[N+1];
+
+        for(int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for (List<Integer> list : graph) {
+            Collections.sort(list);
+        }
+
+        dfs(V);
+        System.out.println(sb.toString().trim());
+
+        // 초기화
+        visited = new boolean[N+1];
+        sb = new StringBuilder();
+
+        bfs(V);
+        System.out.println(sb.toString().trim());
+    }
+}
