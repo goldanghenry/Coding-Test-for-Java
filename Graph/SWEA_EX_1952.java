2f1ab205d24f7e41cb2eba091822d3ec190bb31a
우성현 <wshdhkd@naver.com>
2025-02-27 14:43:34 +0900

    수영장 -> 백트래킹

---
+++ Graph/SWEA_EX_1952.java
@@ -0,0 +1,56 @@
+package Graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class SWEA_EX_1952 {
+    static int result;
+    static int[] tickets, attends;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+
+            st = new StringTokenizer(br.readLine());
+            tickets = new int[4];
+            for (int i = 0; i < 4; i++) {
+                tickets[i] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            attends = new int[12];
+            for (int i = 0; i <12; i++) {
+                attends[i] = Integer.parseInt(st.nextToken());
+            }
+
+            // dfs
+            result = tickets[3]; // 1년 이용권을 기본으로
+            dfs(0,0);
+
+            System.out.println("#"+t+" "+result);
+        }
+    }
+
+    private static void dfs(int depth, int cost) {
+        // 기저 조건
+        if (depth > 12) {
+            result = Math.min(result, cost);
+            return;
+        }
+
+        // 백트래킹
+        if (cost >= result) return;
+
+        // 1일 이용권
+        dfs(depth+1, cost+tickets[0]*attends[depth]);
+
+        // 1달 이용권
+        dfs(depth+1, cost + tickets[1]);
+
+        // 3달 이용권
+        dfs(depth+3, cost + tickets[2]);
+    }
+}
