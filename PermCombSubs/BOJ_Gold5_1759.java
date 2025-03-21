9e9cfe3fc22fcdb25a46869e73c55b7f717a313b
goldanghenry <wshdhkd@naver.com>
2025-03-21 16:19:10 +0900

    BOJ 1758 Gold 5, 암호 만들기

---
+++ PermCombSubs/BOJ_Gold5_1759.java
@@ -0,0 +1,22 @@
+package PermCombSubs;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * BOJ 1758 Gold 5, 암호 만들기
+ */
+
+public class BOJ_Gold5_1759 {
+    static char[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int L = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        arr = new char[C];
+
+    }
+}
