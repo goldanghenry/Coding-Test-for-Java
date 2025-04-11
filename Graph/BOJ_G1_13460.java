8585a8cc2f17c043586ae5af86949df7a12d5ca2
goldanghenry <kwonsh4396@gmail.com>
2025-04-11 17:52:35 +0900

    BOJ 13460 Gold 1, 구슬 탈출 2

---
+++ Graph/BOJ_G1_13460.java
@@ -0,0 +1,17 @@
+package Graph;
+
+/*
+ * BOJ 13460 Gold 1, 구슬 탈출 2
+ * https://www.acmicpc.net/problem/13460
+ * N * M, 보드에는 구멍이 하나, 빨간 구슬과 파란 구설의 크기는 1개로 하나씩 들어있다.
+ * 빨간 구슬을 구멍에서 빼내는 것, 파란 구슬이 구멍에 들어가서는 안 된다.
+ * 중력을 이용해서 이리 저리 굴려야 한다.
+ * 왼쪽, 오른쪽, 위쪽, 아래쪽으로 기울이기 가능
+ * 공은 동시에 움직인다. 
+ */
+
+public class BOJ_G1_13460 {
+    public static void main(String[] args) {
+        
+    }
+}
